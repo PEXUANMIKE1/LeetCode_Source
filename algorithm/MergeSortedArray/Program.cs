8ab922b8abca0430180dc0a598a8a5feefc34e35
Vũ Phi Trường <pexuanmike@gmail.com>
2024-10-16 01:19:20 +0700

    update solution

--- algorithm/MergeSortedArray/Program.cs
+++ algorithm/MergeSortedArray/Program.cs
@@ -1,9 +1,8 @@
-﻿namespace Merge_Sorted_Array
+﻿namespace MergeSortedArray
 {
     public class Program
     {
-        /* 
-        88. Merge Sorted Array
+        /* 88. Merge Sorted Array
       
         You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 
