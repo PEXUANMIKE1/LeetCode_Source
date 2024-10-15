0320b025b5c2763317108837c50c935cd51f1868
Vũ Phi Trường <pexuanmike@gmail.com>
2024-10-16 01:35:45 +0700

    update solution

---
+++ algorithm/Array/MergeSortedArray/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace MergeSortedArray
+{
+    public class Program
+    {
+        /* 88. Merge Sorted Array
+      
+        You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+        Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+        The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+        
+        Example 1:
+
+        Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+        Output: [1,2,2,3,5,6]
+        Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+        The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+        */
+        public void merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            /*
+            Ý tưởng của tôi cho bài này là vừa xắp xếp vừa gộp vào luôn 
+            - Đầu tiên là tạo các biến index của mảng 1, mảng 2 và vị trí đang xắp xếp (xếp từ cuối lên)
+            - lặp luôn trên mảng 1 vì nó dài nhất.
+                so sánh giá trị của 2 phần từ cuối của 2 mảng (không tính phần giá trị 0 ở mảng 1)
+                thằng nào lớn hơn thì đưa nó vào cuối mảng 1, rồi giảm dần index của nó và giảm luôn index đang xắp xếp.
+            - Sau khi lặp hết mảng 1 mà mảng 2 vẫn còn giá trị chưa được đưa vào thì lặp tiếp các
+                phần tử còn lại trong mảng 2 rồi đưa lần lượt chúng vào vị trí index đang xắp xếp
+            */
+
+            int i = m - 1;
+            int j = n - 1;
+            int k = m + n - 1;
+            while (i >= 0 && j >= 0)
+            {
+                if (nums1[i] > nums2[j])
+                {
+                    nums1[k] = nums1[i];
+                    i--;
+                }
+                else
+                {
+                    nums1[k] = nums2[j];
+                    j--;
+                }
+                k--;
+            }
+            while (j >= 0)
+            {
+                nums1[k] = nums2[j];
+                j--;
+                k--;
+            }
+        }
+        public void outputArray(int[] arr, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(i == n - 1 ? arr[i] : arr[i] + ",");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+            int[] nums2 = { 2, 5, 6 };
+            int n = 3;
+            program.merge(nums1, m, nums2, n);
+            program.outputArray(nums1, n + m);
+        }
+    }
+}
---
+++ algorithm/Array/RemoveDuplicateSortedArray/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace RemoveDuplicateSortedArray
+{
+    public class Program
+    {
+        /*      26. Remove Duplicates from Sorted Array
+                - Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element 
+                    appears only once.The relative order of the elements should be kept the same.Then return the number of unique elements in nums.
+                - Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+                - Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums 
+                    initially.The remaining elements of nums are not important as well as the size of nums.
+                -> Return k.
+
+                example:
+                Input: nums = [0,0,1,1,1,2,2,3,3,4]
+                Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+                Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+                It does not matter what you leave beyond the returned k (hence they are underscores).
+
+         */
+
+
+        public int RemoveDuplicate(int[] nums)
+        {
+            /* Solution:
+                Phương án giải quyết: 
+                - Nếu mảng rỗng trả về 0, mảng có 1 phần tử trả về 1 luôn - k phải xét làm gì cho mệt;
+                - Đầu tiên khởi tạo k = 1 (xét 1 mảng k rỗng thì ít nhất mảng phải có 1 phần tử là duy nhất) 
+                - chạy vòng lặp bắt đầu từ phần tử thứ 2 (ở vị trí k luôn - k đóng vai trò là index nằm ngay sau phần tử duy nhất cuối cùng của mảng,
+                                                        do mảng bắt đầu bằng 0 nên k sẽ là số lượng phần tử duy nhất luôn)
+                    nếu phần tử đang xét arr[i] mà khác với phần tử trước đó arr[i-1] thì gán giá trị khác đó cho phần tử ở vị trí k arr[k] và k++
+                    (mục đích là dồn hết các phần tử là duy nhất (không trùng lặp) đứng cạnh nhau, gặp phần tử trùng lặp thì nhảy qua trong khi đó k 
+                        đứng ngay sau danh sách phần tử duy nhất để chờ xem có thằng nào khác thì lại gán giá trị vào vị trí k tiếp)
+
+                *Điều kiện: Mảng đã được xắp xếp tăng dần (cái này array input của bài test kết quả đã xắp xếp sẵn cho rồi nên k cần xắp xếp nữa)
+            */
+
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return 1;
+            int k = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+            }
+            return k;
+        }
+
+        static void Main(string[] args)
+        {
+            Program example1 = new Program();
+            int[] nums1 = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int[] nums2 = { 1, 1, 2 };
+            Console.WriteLine(example1.RemoveDuplicate(nums1));
+            Console.WriteLine(example1.RemoveDuplicate(nums2));
+        }
+    }
+}
